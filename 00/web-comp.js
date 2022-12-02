03f51d4a3b6e6c6e794e361104c92200feab08cb
Joshua Holloway <jhollow6@asu.edu>
2022-12-02 11:48:34 -0600

    Custom elements

---
+++ 00/web-comp.js
@@ -0,0 +1,31 @@
+const innerHTML = `
+  <style>
+    .wc-btn {
+      background: lightgreen;
+      display: inline-block;
+      padding: 1rem;
+      cursor: pointer;
+    }
+  </style>
+  <div class="wc-btn">HTML Element</div>
+`;
+
+// Step 1: Define
+class CustomElement extends HTMLElement {
+
+  constructor() {
+    super();
+
+    // Encapsulate CSS:
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = innerHTML;
+  }
+
+  connectedCallback() {
+    // this.innerHTML = innerHTML;
+  }
+
+}
+
+// Step 2: Register
+window.customElements.define('web-comp', CustomElement);
