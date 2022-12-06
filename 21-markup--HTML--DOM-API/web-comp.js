5ccfdcafd3f2203e5a4dc418195edc21b45dddaa
Joshua Holloway <jhollow6@asu.edu>
2022-12-06 17:48:53 -0600

    markup - DOM API

--- 21-markup--HTML--DOM-API/web-comp.js
+++ 21-markup--HTML--DOM-API/web-comp.js
@@ -1,31 +1,21 @@
-const innerHTML = `
-  <!-- <style>
-    .wc-btn {
-      background: lightgreen;
-      display: inline-block;
-      padding: 1rem;
-      cursor: pointer;
-    }
-  </style> -->
-  <div class="wc-btn">Custom Element</div>
-`;
+// ==============================================
 
 // Step 1: Define
 class CustomElement extends HTMLElement {
 
   constructor() {
     super();
-
-    // Encapsulate CSS:
-    this.attachShadow({ mode: 'open' });
-    this.shadowRoot.innerHTML = innerHTML;
   }
 
   connectedCallback() {
-    // this.innerHTML = innerHTML;
+    const div = document.createElement('div');
+    div.innerText = 'Custom Element';
+    this.appendChild(div);
   }
 
 }
+
+// ==============================================
 
 // Step 2: Register
 window.customElements.define('web-comp', CustomElement);
